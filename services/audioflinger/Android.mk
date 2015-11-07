2a5608118cd12e8a15b71727dc4412340a406550
Adriano Martins <adrianomartins@gmail.com>
2015-11-07 10:40:21 -0200

    Fix build with O3 and GCC 5.2

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -70,6 +70,8 @@ LOCAL_STATIC_LIBRARIES := \
     libscheduling_policy \
     libcpustats \
     libmedia_helper
+    
+LOCAL_CFLAGS += -Os
 
 LOCAL_MODULE:= libaudioflinger
 LOCAL_32_BIT_ONLY := true
