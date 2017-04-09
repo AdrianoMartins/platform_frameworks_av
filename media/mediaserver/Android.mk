cae24f44a82e722cd181ebea4cd89374a527dd74
Adrian DC <radian.dc@gmail.com>
2017-04-09 11:02:19 +0200

    mediaserver: Update mediaserver HALv1 dependencies for O
     * Add libcameraservice shared library dependency
     * Add new android.hardware.camera.common@1.0 and
        android.hardware.camera.provider@2.4 shared
        libraries and exported headers HAL dependencies
    
     * Add libarect static library dependency
    
     * When TARGET_HAS_LEGACY_CAMERA_HAL1 is set, the mediaserver
        includes CameraService.h but exported headers are missing
    
     * Error upon build due to missing libcameraservice linkage:
        fatal error: 'android/hardware/BnCameraService.h' file not found
    
    Change-Id: Ib6bb8a4e9ef18606c64e2dff13504d2eeaac13b1
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- media/mediaserver/Android.mk
+++ media/mediaserver/Android.mk
@@ -40,6 +40,14 @@ LOCAL_CFLAGS := -Werror -Wall
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
     LOCAL_CFLAGS += -DNO_CAMERA_SERVER
+
+    LOCAL_SHARED_LIBRARIES += \
+        libcameraservice \
+        android.hardware.camera.common@1.0 \
+        android.hardware.camera.provider@2.4
+
+     LOCAL_STATIC_LIBRARIES += \
+        libarect
 endif
 
 include $(BUILD_EXECUTABLE)
